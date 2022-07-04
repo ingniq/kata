ebbb3f02cf180ceb0af1d964de57edc28eb8acef
Igor Filatov <siqniq@bk.ru>
2022-07-04 13:42:25 +0300

    DistinctLettersCount implementation

--- distinct_letters_count/distinct_letters_count.py
+++ distinct_letters_count/distinct_letters_count.py
@@ -5,4 +5,18 @@ def solution(s: str) -> int:
     if len(s) > 300000:
         raise ValueError("The length of the string should not exceed 300 000 characters.")
 
-    return 0
+    from collections import Counter
+
+    chars_counter = Counter(s)
+    result = 0
+    _ = {}
+
+    for count in chars_counter.values():
+        try:
+            while count > 0 and _[count]:
+                count -= 1
+                result += 1
+        except KeyError:
+            _[count] = True
+
+    return result
